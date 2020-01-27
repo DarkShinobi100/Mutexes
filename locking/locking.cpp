5cdda08fbeed2bda5afbacb457a4b90217aae75f
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2020-01-27 10:12:42 +0000

    runs add fnction on all 5 threads

--- locking/locking.cpp
+++ locking/locking.cpp
@@ -3,22 +3,43 @@
 
 #include <iostream>
 #include <string>
-
+#include <thread>
 #include "account.h"
 
 // Import things we need from the standard library
 using std::cout;
 using std::endl;
+using std::thread;
 
 Account bill;
+void add()
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+		bill.add(17, 29);
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
-	cout << "Initial: " << bill.total() << "\n";
+	thread myThread[5];
 
-	bill.add(4, 17);
-	bill.add(3, 0);
-	bill.add(10, 99);
+	cout << "Initial: " << bill.total() << "\n";
+	
+	for (int i = 0; i < 5; i++)
+	{
+		myThread[i] = thread(add);
+	}
+	//bill.add(4, 17);
+	//bill.add(3, 0);
+	//bill.add(10, 99);
+	// Wait for myThread to finish.
+
+	for (int i = 0; i < 5; i++)
+	{
+		myThread[i].join();
+	}
 
 	cout << "Total: " << bill.total() << "\n";
 
